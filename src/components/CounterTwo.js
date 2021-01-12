86b0df5635dd84e995b6f5d697ac694e45ed9176
Mubassir-Ali <mubassir11@gmail.com>
2021-01-12 20:32:14 +0500

    Global state '...state'

--- src/components/CounterTwo.js
+++ src/components/CounterTwo.js
@@ -1,14 +1,20 @@
 import React,{useReducer} from 'react'
 
 const initialState={
-    first:0};
+    first:0,
+    second:10
+};
 
 const reducer=(state,action)=>{
     switch(action.type){
         case 'increment':
-            return {first: state.first +1}
+            return {...state, first: state.first +action.value}
         case 'decrement':
-            return {first:state.first -1}
+            return {...state, first: state.first -action.value}
+        case 'increment5':
+            return {...state, second: state.second +action.value}
+        case 'decrement5':
+            return {...state, second: state.second -action.value}
         case 'reset':
             return initialState
     
@@ -21,16 +27,24 @@ export const CounterTwo = () => {
     return (
         <div className="container">
         <div>
-        <h1>{count.first}</h1>
+        <h1>First Counter: {count.first}</h1>
+        <h1>Second Counter: {count.second}</h1>
 
         </div>
         
-    
-   
+  
         <div className='btn'>
-            <button onClick={()=>dispatch({type:'increment'})}>Increment</button>
-            <button onClick={()=>dispatch({type:'decrement'})}>Decrement</button>
+            <button onClick={()=>dispatch({type:'increment', value:1})}>Increment 1</button>
+            <button onClick={()=>dispatch({type:'decrement', value:1})}>Decrement 1</button>
             <button onClick={()=>dispatch({type:'reset'})}>Reset</button>
+       
+
+            <br/>
+       
+            <button onClick={()=>dispatch({type:'increment5', value:5})}>Increment count 5</button>
+            <button onClick={()=>dispatch({type:'decrement5', value:5})}>Decrement count 5</button>
+           
+
         </div>
         </div>
 
