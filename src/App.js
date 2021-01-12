695d08d464633e76066bc30996221b1a8276b79d
Mubassir-Ali <mubassir11@gmail.com>
2021-01-12 22:01:00 +0500

    add Component A,B and C pass data through context Api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
+import React, {useReducer} from 'react'
 import './App.css';
 import Counter from './components/counter'
 import {CounterTwo} from './components/CounterTwo'
+import {ComponentA} from './components/ComponentA'
+import {ComponentB} from './components/ComponentB'
+import {ComponentC} from './components/ComponentC'
+
+export const CountContext =React.createContext()
+
+
+
+const initialState=0;
+
+const reducer=(state,action)=>{
+  switch(action){
+    case 'increment':
+      return state +1
+    case 'decrement':
+      return state -1
+    case 'reset':
+      return initialState
+
+  }
+}
+
 
 
 
 function App() {
+  const [count,dispatch]=useReducer(reducer,initialState)
   return (
-    <div>
-    
-    <CounterTwo/>  
+	  <CountContext.Provider value={{countState:count, countDispatch:dispatch}}>
+		<div>
+		<h1>Counter: {count}</h1>
+
+			<ComponentA/>
+			<ComponentB/>
+			<ComponentC/>
+		</div>
+
+	  </CountContext.Provider>
+
    
-    </div>
+	
+	
+
+   
+   
+  
   );
 }
 
---
+++ src/components/ComponentB.js
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react';
+import {CountContext} from '../App';
+
+
+export const ComponentB = () => {
+    const countContext =useContext(CountContext)
+
+    return (
+        <div className="container">
+              
+        <div className='btn'>
+            ComponentB
+            <button onClick={()=>countContext.countDispatch('increment')}>Increment</button>
+            <button onClick={()=>countContext.countDispatch('decrement')}>Decrement</button>
+            <button onClick={()=>countContext.countDispatch('reset')}>Reset</button>
+        </div>
+    </div>
+    )
+}
